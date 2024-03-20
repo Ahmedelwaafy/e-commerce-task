ba797fddf92892dc166e890d7fc753382c154f44
Ahmedelwaafy <ahmedelwaafy@gmail.com>
2024-03-20 17:22:18 +0200

    deploying

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/e-commerce-task/",
   plugins: [react()],
   resolve: {
     alias: {
